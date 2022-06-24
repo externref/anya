f5fd40f4c1ccf06a25a2ef7121c9a3bd82791cb7
sarthhh <shiva02939@gmail.com>
2022-06-24 18:33:50 +0530

    docs, full setup

--- handlers/greetings_handler.py
+++ handlers/greetings_handler.py
@@ -1,8 +1,16 @@
+from __future__ import annotations
+
+import typing as t
+
 import hikari
 import lightbulb
 
+__all__: t.Tuple[str, ...] = ("GreetingsMethods",)
 
-class GreetingsHandler:
+
+class GreetingMethods:
+    """Handler for welcome and goodbye messages."""
+
     @staticmethod
     async def set_channel(
         context: lightbulb.Context,
@@ -47,7 +55,7 @@ class GreetingsHandler:
         context: lightbulb.Context, greeting: str, message: str
     ) -> None:
         """
-        Updating the welcome message in the database.
+        Updating the greeting message in the database.
 
         Paramaters
         ----------
@@ -57,21 +65,50 @@ class GreetingsHandler:
             greeting: :class:`str`
                 Type of greeting ( welcome/ goodbye )
             message: :class:`str`
-                The mew message to set.
+                The new message to set.
 
         """
-        await context.bot.greeting_db.update_message_for(  # type: ignore
+
+        context.bot.greeting_db.update_message_for(  # type: ignore
             greeting, context.guild_id, message
         )
         await context.respond(
             embed=hikari.Embed(
-                description=message, color=context.bot.colors.green  # type: ignore
-            ).set_author(name="Set welcome message to:"),
+                description=f"Set welcome message to: ```\n{message}```", color=context.bot.colors.green  # type: ignore
+            ),
             reply=True,
         )
 
     @staticmethod
-    async def set_welcome_bytes(
+    async def set_color(
+        context: lightbulb.Context, greeting: str, new_color: int
+    ) -> None:
+        """
+        Updating the greeting color in the database.
+
+        Paramaters
+        ----------
+
+            context: :class:`lightbulb.Context`
+                The context which called the command.
+            greeting: :class:`str`
+                Type of greeting ( welcome/ goodbye )
+            new_color: :class:`int`
+                Hex/Int value of the color to set.
+
+
+        """
+        await context.bot.greeting_db.update_color_for(  # type: ignore
+            greeting, context.guild_id, new_color
+        )
+        await context.respond(
+            embed=hikari.Embed(
+                description=f"Set {greeting} color to `{new_color}`", color=new_color  # type: ignore
+            )
+        )
+
+    @staticmethod
+    async def set_bytes(
         context: lightbulb.Context, greeting: str, data_bytes: bytes
     ) -> None:
         """
@@ -91,4 +128,33 @@ class GreetingsHandler:
 
         await context.bot.greeting_db.set_image_for(  # type: ignore
             greeting, context.guild_id, data_bytes
+        )
+
+    @staticmethod
+    async def toggle_embed(
+        context: lightbulb.Context, greeting: str, toggle: bool = True
+    ) -> None:
+        """
+        Toggle between embed and text type log messages.
+
+        Paramaters
+        ----------
+
+            context: :class:`lightbulb.Context`
+                The context which called the command.
+            greeting: :class:`str`
+                Type of greeting.
+            toggle: :class:`bool`
+                Embed value: True/False.
+
+        """
+
+        await context.bot.greeting_db.set_embed_option(  # type: ignore
+            greeting, context.guild_id, toggle
+        )
+        await context.respond(
+            embed=hikari.Embed(
+                description=f"Messages will now appear as `{'embed' if toggle is True else 'text'}` messages",
+                color=context.bot.colors.green,  # type: ignore
+            )
         )
